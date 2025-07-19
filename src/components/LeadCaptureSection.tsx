d7122c8cf243b8ac0466db1b45332edb0a09c4ec
jeffpodmayer <jeff.podmayer@gmail.com>
2025-07-18 20:23:04 -0700

    upated mobile layout

--- src/components/LeadCaptureSection.tsx
+++ src/components/LeadCaptureSection.tsx
@@ -17,11 +17,14 @@ export function LeadForm({
 }: LeadFormProps) {
   return (
     <section className="relative">
-      <div className="absolute inset-0 bg-primary-lightest h-[70%]"></div>
+      <div
+        className="absolute inset-0 bg-primary-lightest"
+        style={{ height: "70%" }}
+      ></div>
 
       <div className="relative z-10">
         <div className="max-w-3xl mx-auto bg-white rounded-3xl shadow-xl overflow-hidden mx-4 md:mx-auto">
-          <div className="bg-primary-light grid md:grid-cols-3 gap-4 md:gap-8 p-6 rounded-t-2xl">
+          <div className="bg-primary-light grid md:grid-cols-3 gap-4 md:gap-8 p-6 rounded-t-2xl text-center justify-items-center">
             <SellingPoint text="US-based agents" />
             <SellingPoint text="No pushy salespeople" />
             <SellingPoint text="No spam calls, EVER." />
--- src/components/SellingPoint.tsx
+++ src/components/SellingPoint.tsx
@@ -6,8 +6,8 @@ interface SellingPointProps {
 
 export function SellingPoint({ text }: SellingPointProps) {
   return (
-    <div className="text-center">
-      <div className="flex items-center justify-center gap-2">
+    <div className="text-center justify-center">
+      <div className="flex items-center gap-2">
         <img src={checkmark} alt="checkmark" className="h-4 w-4" />
         <h3 className="text-lg font-semibold text-white">{text}</h3>
       </div>
